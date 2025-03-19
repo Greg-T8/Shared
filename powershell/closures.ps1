e6a3ee58da5b9467c5e6a0d1c490fef1d61d5dd1
Greg Tate <gregt8@outlook.com>
2025-03-19 04:52:24 -0500

    Refactor logging functionality in closures.ps1 to improve structure and usability

--- powershell/closures.ps1
+++ powershell/closures.ps1
@@ -19,69 +19,54 @@
     This technique is particularly useful for creating reusable function factories where you want to
 #>
 
-test
+$Main = {
+
+    # Initialize logging
+    $logHeaders = 'User', 'Property', 'Value', 'Message'
+    $function:Log = Logger -LogName UserUpdate -Headers $logHeaders -LogType Information
+
+    $logHeaders =  'Action', 'Message'
+    $function:LogEx = Logger -LogName UserUpdateException -Headers $logHeaders -LogType Exception
+
+    # Example usage of the logging functions
+    Log @{ User = 'john'; Property = 'DisplayName'; Value = 'John Doe'; Message = 'User display name updated' }
+    LogEx @{ Action = 'Establish Connection'; Message = 'Issue connecting to database' }
+
+}
 
 function Logger {
     [OutputType([ScriptBlock])]
     param (
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
-        # No whitespace allowed in log name
-        [ValidateScript({ -not ($_ -match '^\s|\s$') -and -not ($_ -match '\s') })]
         [string]$LogName,
-
-        [Parameter(Mandatory)]
-        [ValidateNotNullOrEmpty()]
         [string[]]$Headers,
-
-        [ValidateSet('Information', 'Exception')]
-        [string]$LogType = 'Information',
-
-        [switch]$SuppressHostOutput,
-        [switch]$SuppressLogOutput
+        [string]$LogType = 'Information'
     )
 
-    $logDirectory = Join-Path -Path $PSScriptRoot -ChildPath 'Output'
-    if (-not (Test-Path -Path $logDirectory)) {
-        New-Item -Path $logDirectory -ItemType Directory | Out-Null
-    }
+    # Set up the log file path, but do not create the file yet
     $logFileName = "$(Get-Date -Format 'yyyy-MM-dd-HHmmss')_$($LogName).csv"
-    $logFile = Join-Path -Path $logDirectory -ChildPath $logFileName
+    $logFile     = Join-Path -Path $PSScriptRoot -ChildPath $logFileName
+
+    # Set up a Write-Host function based on the log type, but do not call it yet.
+    # $Message is only a placeholder at this point; it does not exist as $WriteHost is not called yet.
     switch ($LogType) {
         'Information' { $WriteHost = { param($Message) Write-Host -Message $Message -ForegroundColor Gray } }
         'Exception' { $WriteHost = { param($Message) Write-Host -Message $Message -ForegroundColor Yellow } }
     }
+
     return {
-        [OutputType($null)]
         param (
-            [Parameter(Mandatory, Position = 0)]
-            [ValidateScript(
-                {
-                    # Validate that the object passed to the logger has the same properties as the headers
-                    $passedProperties = $_.PSObject.Properties.Name
-                    $result = Compare-Object -ReferenceObject $Headers -DifferenceObject $passedProperties
-                    return $result.Count -eq 0
-                },
-                ErrorMessage = 'The item following item did not pass validation for logging: {0}.' +
-                'Ensure the properties of the logging object contain each of the defined log headers.'
-            )]
-            [PSCustomObject]$LogObject
+            [Parameter(Mandatory = $true)]
+            [hashtable]$LogObject
         )
-        if (-not $SuppressHostOutput) {
-            # Then write to host
-            $logObject | Format-List | Out-String | ForEach-Object { $_ -split "`n" } |
-                Where-Object { $_ -ne '' -and $_ -ne $null } | ForEach-Object { & $WriteHost $_ }
-        }
-        if (-not $SuppressLogOutput) {
-            # Then write to log file
-            $csvEncoding = 'utf8BOM'
-            if (-not (Test-Path -Path $logFile)) {
-                $Headers -join ',' | Out-File -FilePath $logFile -Encoding $csvEncoding
-            }
-            $logObject | Export-Csv -Path $logFile -Append -Force -NoTypeInformation -Encoding $csvEncoding
-        }
+
+        # Write the log object to the console host
+        [PSCustomObject]$logObject | Format-List | Out-String | ForEach-Object { & $WriteHost $_ }
+
+        # Write the log object to the CSV file
+        if (-not (Test-Path -Path $logFile)) { $Headers -join ',' | Out-File -FilePath $logFile }
+        [PSCustomObject]$logObject | Export-Csv -Path $logFile -Append -Force -NoTypeInformation
+
     }.GetNewClosure()
-    # Use a scriptblock closure to capture or "close" variables that are defined in the outer funcion, i.e. the
-    # parent function before before the returned scriptblock; otherwise the returned scriptblock will not be
-    # able to access variables from the outer function..
 }
+
+& $Main
